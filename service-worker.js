3d034e5ee34e8fdf6903e7425ea988a2cd5c191e
FeDiasEdu <134555312+FeDiasEdu@users.noreply.github.com>
2026-02-21 01:27:24 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ==========================
 // VERSIONAMENTO
 // ==========================
-const APP_VERSION = "v8";
+const APP_VERSION = "v9";
 const CACHE_NAME = `fitness-app-${APP_VERSION}`;
 
 // ==========================
